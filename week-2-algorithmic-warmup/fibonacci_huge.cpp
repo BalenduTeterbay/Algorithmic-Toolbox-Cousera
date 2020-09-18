fa4ce30fee3f9ccce43aa095aa034653beb086f3
BalenduTeterbay <70392787+BalenduTeterbay@users.noreply.github.com>
2020-09-18 18:06:13 +0530

    Add files via upload

---
+++ week-2-algorithmic-warmup/fibonacci_huge.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll pisanoperiod(ll m)
+{
+  ll a=0,b=1,c=a+b;
+  for(ll i=0;i<m*m;i++)
+   {
+     c=(a+b)%m;
+     a=b;
+     b=c;
+    if(a==0&&b==1)return i+1;
+    }
+}
+ll fibohuge(ll n,ll m)
+{
+  ll remainder=n%pisanoperiod(m);
+  ll a=0,b=1,c=remainder;
+  for(ll i=1;i<remainder;i++)
+   {
+     c=(a+b)%m;
+     a=b;
+     b=c;
+    }
+return c%m;
+}
+int main() {
+    ll n, m;
+    cin >> n >> m;
+    cout << fibohuge(n, m) << '\n';
+}
